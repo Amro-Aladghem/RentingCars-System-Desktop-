6f7bc4581b4e1e8dc46c75de25956d40da50804e
Your Name <ameraladghem@gmail.com>
2024-08-23 22:41:40 +0300

    starting with Renting Stage

---
+++ clsBuesnesLayer/clsScheduling.cs
@@ -0,0 +1,208 @@
+﻿using clsDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace clsBuesnesLayer
+{
+    public class clsScheduling
+    {
+        public enum eSchedulingStatus { Active = 1, Cancled = 2, Completed = 3 };
+
+        enum eMode { AddNew = 1, Update = 2 };
+        eMode Mode;
+
+        public int ScheduleID { get; set; }
+        public int CustomerID { get; set; }
+        public int VehicleID { get; set; }
+        public DateTime DateOfSechedule { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime StartFrom { get; set; }
+        public DateTime EndAt { get; set; }
+        public int Status { get; set; }
+        public int? DriverID { get; set; }
+
+
+        public clsScheduling()
+        {
+
+            this.ScheduleID = -1;
+            this.CustomerID = -1;
+            this.VehicleID = -1;
+            this.DateOfSechedule = DateTime.Now;
+            this.NumberOfDays = 0;
+            this.StartFrom = DateTime.Now;
+            this.EndAt = DateTime.Now.AddDays(1);
+            this.Status = 1;
+            this.DriverID = -1;
+
+            Mode = eMode.AddNew;
+        }
+
+        public clsScheduling(int ScheduleID, int CustomerID, int VehicleID, DateTime DateOfSechedule, int NumberOfDays, DateTime StartFrom, DateTime EndAt, int Status, int? DriverID)
+        {
+            this.ScheduleID = ScheduleID;
+            this.CustomerID = CustomerID;
+            this.VehicleID = VehicleID;
+            this.DateOfSechedule = DateOfSechedule;
+            this.NumberOfDays = NumberOfDays;
+            this.StartFrom = StartFrom;
+            this.EndAt = EndAt;
+            this.Status = Status;
+            this.DriverID = DriverID;
+
+            Mode = eMode.Update;
+        }
+
+        public static clsScheduling FindSchedulingByID(int SchedulingID)
+        {
+            int CustomerID = -1, VehicleID = -1, NumberOfDays = -1, Status = -1; int? DriverID = -1;
+            DateTime DateOfSechedule = DateTime.Now, StartFrom = DateTime.Now, EndAt = DateTime.Now;
+
+            if (clsSchedulingData.GetFullRecordByID(SchedulingID, ref CustomerID, ref VehicleID, ref DateOfSechedule, ref NumberOfDays, ref StartFrom, ref EndAt, ref Status, ref DriverID))
+            {
+                return new clsScheduling(SchedulingID, CustomerID, VehicleID, DateOfSechedule, NumberOfDays, StartFrom, EndAt, Status, DriverID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static clsScheduling FindSchedulingByCustomerName(string Firstname, string Lastname)
+        {
+
+            int SchedulingID = -1, CustomerID = -1, VehicleID = -1, NumberOfDays = -1, Status = -1; int? DriverID = -1;
+            DateTime DateOfSechedule = DateTime.Now, StartFrom = DateTime.Now, EndAt = DateTime.Now;
+
+            if (clsSchedulingData.GetFullRecordByCustomerName(Firstname, Lastname, ref SchedulingID, ref CustomerID, ref VehicleID, ref DateOfSechedule, ref NumberOfDays, ref StartFrom, ref EndAt, ref Status, ref DriverID))
+            {
+                return new clsScheduling(SchedulingID, CustomerID, VehicleID, DateOfSechedule, NumberOfDays, StartFrom, EndAt, Status, DriverID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static clsScheduling FindSchedulingByCustomerNameAndVehicleID(string FirstName, string LastName, int VehicleID)
+        {
+            int SchedulingID = -1, CustomerID = -1, NumberOfDays = -1, Status = -1; int? DriverID = -1;
+            DateTime DateOfSechedule = DateTime.Now, StartFrom = DateTime.Now, EndAt = DateTime.Now;
+
+            if (clsSchedulingData.GetFullRecrodByCustomerNameAndVehicleID(FirstName, LastName, ref SchedulingID, ref CustomerID, VehicleID, ref DateOfSechedule, ref NumberOfDays, ref StartFrom, ref EndAt, ref Status, ref DriverID))
+            {
+                return new clsScheduling(SchedulingID, CustomerID, VehicleID, DateOfSechedule, NumberOfDays, StartFrom, EndAt, Status, DriverID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private bool _AddNewScheduling()
+        {
+            this.VehicleID = clsSchedulingData.AddNewSchedule(this.CustomerID, this.VehicleID, this.DateOfSechedule, this.NumberOfDays, this.StartFrom, this.EndAt, this.Status, this.DriverID);
+            return VehicleID != -1;
+        }
+
+        private bool _UpdateScheduling()
+        {
+            return clsSchedulingData.UpdateSchedule(this.ScheduleID, this.VehicleID, this.NumberOfDays, this.StartFrom, this.EndAt, this.Status, this.DriverID);
+        }
+
+        public bool Save()
+        {
+
+            switch (Mode)
+            {
+
+                case eMode.AddNew:
+                    {
+                        if (_AddNewScheduling())
+                        {
+                            Mode = eMode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case eMode.Update:
+                    {
+
+                        if (_UpdateScheduling())
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                default: return false;
+            }
+
+
+
+
+
+        }
+
+        public static DataTable GetAllSchedulingInSystem()
+        {
+            return clsSchedulingData.GetAllRecordInSystem();
+        }
+
+        public static bool DeleteScheduling(int SchedulingID)
+        {
+            return clsSchedulingData.DeleteSchedule(SchedulingID);
+        }
+
+        public bool DeleteScheduling()
+        {
+            return clsSchedulingData.DeleteSchedule(this.ScheduleID);
+        }
+
+        public static bool ChangTheStatusOfScheduling(int SchedulingID,int NewStatus)
+        {
+            return clsSchedulingData.ChangeTheStatusOfSchedule(SchedulingID, NewStatus);
+        }
+
+        public static bool IsCustomerHasMoreOneScheduling(int CustomerID)
+        {
+            return clsSchedulingData.CheckIfCustomerHasAndActiveSchedule(CustomerID);
+        }
+
+        public static bool IsCustomerHasMoreOneScheduling(string FirstName,string LastName)
+        {
+            return clsSchedulingData.CheckIfCustomerHasAndActiveSchedule(FirstName, LastName);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ clsDataAccessLayer/clsSchedulingData.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clsDataAccessLayer
+{
+    public class clsSchedulingData
+    {
+
+        private static void AddValueToCommandParam(SqlCommand command,string ParamName,object value)
+        {
+            if (value == null)
+            {
+                command.Parameters.AddWithValue(ParamName,System.DBNull.Value);
+            }
+            else
+            {
+                command.Parameters.AddWithValue(ParamName,value);
+            }
+        }
+
+        public static DataTable GetAllRecordInSystem()
+        {
+            DataTable dt=new DataTable();
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using(SqlCommand command=new SqlCommand("SP_GetAllSchedules",connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using(SqlDataReader reader=command.ExecuteReader())
+                        {
+                            if(reader.HasRows)
+                            {
+                                dt.Load(reader);
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return dt;
+           
+        }
+
+        public static bool GetFullRecordByID(int ScheduleID,ref int CustomerID,ref int VehicleID,ref DateTime DateOfSchedule,ref int NumberOfDay,ref DateTime StartDate,ref DateTime EndDate,ref int StatusID,ref int?DriverID)
+        {
+            bool isFound = false;
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string query = " select *from Schedules where ScheduleID=@ScheduleID;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ScheduleID", ScheduleID);
+
+                        using(SqlDataReader reader=command.ExecuteReader())
+                        {
+                            if(reader.Read())
+                            {
+                                isFound = true;
+
+                                CustomerID = (int)reader[1];
+                                VehicleID = (int)reader[2];
+                                DateOfSchedule = (DateTime)reader[3];
+                                NumberOfDay = (int)reader[4];
+                                StartDate = (DateTime)reader[5];
+                                EndDate = (DateTime)reader[6];
+                                StatusID = (int)reader[7];
+                                DriverID = (int)reader[8];
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isFound;
+        }
+
+        public static bool GetFullRecordByCustomerName(string FirstName,string LastName,ref int ScheduleID, ref int CustomerID, ref int VehicleID, ref DateTime DateOfSchedule, ref int NumberOfDay, ref DateTime StartDate, ref DateTime EndDate, ref int StatusID, ref int? DriverID)
+        {
+            bool isFound = false;
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using(SqlCommand command=new SqlCommand("SP_FindScheduleByCustomerName",connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@FirstName", FirstName);
+                        command.Parameters.AddWithValue("@LastName", LastName);
+
+                        using(SqlDataReader reader=command.ExecuteReader())
+                        {
+                            if(reader.Read())
+                            {
+                                isFound = true;
+
+                                ScheduleID = (int)reader[0];
+                                CustomerID = (int)reader[1];
+                                VehicleID = (int)reader[2];
+                                DateOfSchedule = (DateTime)reader[3];
+                                NumberOfDay = (int)reader[4];
+                                StartDate = (DateTime)reader[5];
+                                EndDate = (DateTime)reader[6];
+                                StatusID = (int)reader[7];
+                                DriverID = (int)reader[8];
+                            }
+
+
+                        }
+
+                    }
+
+                }
+
+            }
+            catch
+            {
+                //
+            }
+
+            return isFound;
+        }
+
+        public static bool GetFullRecrodByCustomerNameAndVehicleID(string FirstName, string LastName, ref int ScheduleID, ref int CustomerID,  int VehicleID, ref DateTime DateOfSchedule, ref int NumberOfDay, ref DateTime StartDate, ref DateTime EndDate, ref int StatusID, ref int? DriverID)
+        {
+            bool isFound = false;
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using(SqlCommand command=new SqlCommand("SP_FindScheduleByVehicleOfCustomer",connection))
+                    {
+                        command.Parameters.AddWithValue("@FirstName", FirstName);
+                        command.Parameters.AddWithValue("@LastName", LastName);
+                        command.Parameters.AddWithValue("@VehcileID", VehicleID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                isFound = true;
+
+                                ScheduleID = (int)reader[0];
+                                CustomerID = (int)reader[1];
+                                VehicleID = (int)reader[2];
+                                DateOfSchedule = (DateTime)reader[3];
+                                NumberOfDay = (int)reader[4];
+                                StartDate = (DateTime)reader[5];
+                                EndDate = (DateTime)reader[6];
+                                StatusID = (int)reader[7];
+                                DriverID = (int)reader[8];
+
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch
+            {
+                //
+            }
+
+            return isFound;
+        }
+
+        public static int AddNewSchedule(int CustomerID, int VehicleID, DateTime DateOfSchedule, int NumberOfDay, DateTime StartDate, DateTime EndDate, int StatusID, int? DriverID)
+        {
+            int NewID = -1;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string query = "Insert Into Schedules(CustomerID,VehicleID,DateOfSchedule,NumberOfDay,StartFrom,EndAt,status,DriverID)\r\nvalues\r\n" +
+                        "(@CustomerID,@VehicleID,@DateOfSchedule,@NumberOfDay,@StartFrom,@EndAt,@status,@DriverID);select SCOPE_IDENTITY()";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        command.Parameters.AddWithValue("@VehicleID", VehicleID);
+                        command.Parameters.AddWithValue("@DateOfSchedule", DateOfSchedule);
+                        command.Parameters.AddWithValue("@NumberOfDay", NumberOfDay);
+                        command.Parameters.AddWithValue("@StartFrom", StartDate);
+                        command.Parameters.AddWithValue("@EndAt", EndDate);
+                        command.Parameters.AddWithValue("@status", StatusID);
+                        AddValueToCommandParam(command, "@DriverID", DriverID);
+
+                        object reader = command.ExecuteScalar();
+                        if (reader != null && int.TryParse(reader.ToString(), out int value))
+                        {
+                            NewID = value;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return NewID;
+        }
+
+        public static bool UpdateSchedule(int ScheduleID,int VehicleID,int NumberOfDays,DateTime StartFrom,DateTime EndAt,int statusID,int ?DriverID)
+        {
+            bool isDone = false;
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string query = " Update Schedules \r\n set VehicleID=@VehicleID ,DateOfSchedule=GETDATE(), NumberOfDay=@NumberOfDay,StartFrom=@StartFrom , EndAt=@EndAt ,status=@status , DriverID=@DriverID where ScheduleID=@ScheduleID;";
+
+                    using(SqlCommand command=new SqlCommand(query,connection))
+                    {
+                        command.Parameters.AddWithValue("@ScheduleID", ScheduleID);
+                        command.Parameters.AddWithValue("@VehicleID", VehicleID);
+                        command.Parameters.AddWithValue("@NumberOfDay", NumberOfDays);
+                        command.Parameters.AddWithValue("@StartFrom", StartFrom);
+                        command.Parameters.AddWithValue("@EndAt", EndAt);
+                        command.Parameters.AddWithValue("@status", statusID);
+                        AddValueToCommandParam(command, "@DriverID", DriverID);
+
+                        int rowaffected = command.ExecuteNonQuery();
+                        if (rowaffected > 0)
+                        {
+                            isDone = true;
+                        }
+                    }
+
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isDone;
+        }
+
+        public static bool DeleteSchedule(int ScheduleID)
+        {
+            bool isDone = false;
+
+
+            try
+            {
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string query = " delete Schedules where ScheduleID=@ScheduleID";
+
+                    using(SqlCommand command=new SqlCommand(query,connection))
+                    {
+                        command.Parameters.AddWithValue("@ScheduleID", ScheduleID);
+
+                        int rowaffected = command.ExecuteNonQuery();
+                        if(rowaffected > 0)
+                        {
+                            isDone = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isDone;
+        }
+
+        public static bool ChangeTheStatusOfSchedule(int ScheduleID, int NewStatusID)
+        {
+            bool isDone = false;
+
+            try
+            {
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    string qurey = "Update Schedules \r\n set status=@NewStatus  where ScheduleID=@ScheduleID";
+
+                    using(SqlCommand command=new SqlCommand(qurey,connection))
+                    {
+                        command.Parameters.AddWithValue("@NewStatus", NewStatusID);
+                        command.Parameters.AddWithValue("@ScheduleID", ScheduleID);
+
+                        int rowaffected= command.ExecuteNonQuery();
+                        if (rowaffected > 0)
+                        {
+                            isDone = true;
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isDone;
+        }
+
+        public static bool CheckIfCustomerHasAndActiveSchedule(int CustomerID)
+        {
+
+            bool isRight = false;
+
+            try
+            {
+
+                using(SqlConnection connection=new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = " select Count(CustomerID) from Schedules where CustomerID=@CustomerID And status=1;";
+
+                    using(SqlCommand command=new SqlCommand(query,connection))
+                    {
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+
+                        object reader=command.ExecuteScalar();
+                        if (reader != null && int.TryParse(reader.ToString(),out int value))
+                        {
+                            isRight = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isRight;
+        }
+
+        public static bool CheckIfCustomerHasAndActiveSchedule(string FirstName,string LastName)
+        {
+
+            bool isRight = false;
+
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = " SELECT COUNT(Schedules.CustomerID) AS Expr1\r\nFROM   Schedules INNER JOIN\r\n             Customers ON Schedules.CustomerID = Customers.CustomerID INNER JOIN\r\n             Persons ON Customers.PersonID = Persons.PersonID\r\n\t\t\t where FirstName=@FirstName And LastName=@LastName ANd status=1;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@FirstName", FirstName);
+                        command.Parameters.AddWithValue("@LastName", LastName);
+
+                        object reader = command.ExecuteScalar();
+                        if (reader != null && int.TryParse(reader.ToString(), out int value))
+                        {
+                            isRight = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                //
+            }
+
+            return isRight;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
